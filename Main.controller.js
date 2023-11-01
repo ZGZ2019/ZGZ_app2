3311b830e93023cd1a369d3c17062f6529d3cda0
GZ Zheng <13620050430@139.com>
2023-11-01 02:27:05 +0000

    first commit

---
+++ Main.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+    'sap/ui/core/mvc/Controller'
+], function(Controller) {
+    'use strict';
+    return Controller.extend("webapp.Main",{
+        //Event handler
+        onPress: function(){
+            //Step 1: get the object of our view
+            var oView = this.getView();
+            //Step 2: get the object of image which is inside the view
+            var oSpidy = oView.byId("idSpiderman");
+            //Step 3: make the image visible
+            oSpidy.setVisible(true);
+        }
+    });
+});
